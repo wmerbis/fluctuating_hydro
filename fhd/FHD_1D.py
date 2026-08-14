Fixed factor 2 in voter noise scaling + added projection_mode redistribute

fhd/FHD_1D.py
@@ -252,7 +252,7 @@ class fhd:
             #     xi2[:,0] = 0
             #     xi2[:,-1] = 0
             rho_face     = np.maximum(phi[0]*phi[1], self.phi_floor**2) # Changed to noise_floor^2 because phi*phi0 is also a square!
-            demographic_noise = np.einsum("i,i->i", np.sqrt(4*param['D_v']*rho_face/dt/h), xi2)
+            demographic_noise = np.einsum("i,i->i", np.sqrt(4*2*param['D_v']*rho_face/dt/h), xi2)
             
             noise_ceiling = np.maximum(-phi[0]/dt,param['noise_v']*demographic_noise)
             noise_floor = np.minimum(noise_ceiling, phi[1]/dt)

structure_factor.py
@@ -9,9 +9,9 @@ def noise_matrix(k, a0, b0, param):
     h = param['h']
     k2 = k**2
     r0 = 1-a0-b0
-    q11 = DA * h**2 * r0 * a0 * k2 + 2.0 * Dv * a0* b0
-    q22 = DB * h**2 * r0 * b0 * k2 + 2.0 * Dv * a0 * b0
-    q12 = -2.0 * Dv * a0* b0
+    q11 = DA * h**2 * r0 * a0 * k2 + 4.0 * Dv * a0* b0
+    q22 = DB * h**2 * r0 * b0 * k2 + 4.0 * Dv * a0 * b0
+    q12 = -4.0 * Dv * a0* b0
     return np.array([[q11, q12],
                      [q12, q22]], dtype=float)
 